0cb87056b6e2e52254d986f96b01ec1eb8e341cf
unknown <devethancraig@gmail.com>
2019-06-24 17:39:00 -0600

    Clear Btn && Destructuring

--- src/App.js
+++ src/App.js
@@ -23,20 +23,28 @@ class App extends Component {
   // }
 
   searchUsers = async text => {
-    this.setState({loading: true});
+    this.setState({ loading: true });
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({
       users: res.data.items,
       loading: false
     });
   }
+
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
+    let { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClearBtn={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,24 +8,36 @@ class Search extends Component {
     }
 
     static propsTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClearBtn: PropTypes.bool.isRequired
     }
-    
+
     handleSubmit = e => {
         e.preventDefault();
         this.props.searchUsers(this.state.text);
-        this.setState({text: ''});
+        this.setState({ text: '' });
     }
 
-    handleChange = e => this.setState({[e.target.name]: e.target.value});
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
     render() {
+        const { showClearBtn, clearUsers } = this.props;
         return (
             <div onSubmit={this.handleSubmit}>
                 <form className="form" >
-                    <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.handleChange} />
+                    <input
+                        type="text"
+                        name="text"
+                        placeholder="Search Users"
+                        value={this.state.text}
+                        onChange={this.handleChange}
+                    />
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+                {showClearBtn && (
+                    <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+                )}
             </div>
         );
     }
